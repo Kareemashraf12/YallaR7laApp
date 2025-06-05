5f449b5ed5ac455dc91178933079368e422c1431
Kareemashraf12 <125606685+Kareemashraf12@users.noreply.github.com>
2025-06-05 17:23:25 +0300

    Update MdlUser.cs

--- YallaR7la/DtoModels/MdlUser.cs
+++ YallaR7la/DtoModels/MdlUser.cs
@@ -5,23 +5,23 @@ namespace YallaR7la.DtoModels
     public class MdlUser
     {
         [MaxLength(100)]
-        public string Name { get; set; }
-        [Required(ErrorMessage = "Email is Required")]
+        public string? Name { get; set; }
+        
         [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email format.")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Password is Required")]
+        public string? Email { get; set; }
+        
         [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",
         ErrorMessage = "Password must be at least 8 characters long, contain one uppercase letter, one lowercase letter, one number, and one special character.")]
         public string Password { get; set; }
-        public string Prefrance { get; set; }
-        public string City { get; set; }
-        [Required(ErrorMessage = "Number is Required")]
+        public string? Prefrance { get; set; }
+        public string? City { get; set; }
+        
         [RegularExpression(@"^(\+201|01|00201)[0-2,5]{1}[0-9]{8}", ErrorMessage = "Invalid phone number format.")]
         
-        public string PhoneNumper { get; set; }
+        public string? PhoneNumper { get; set; }
         public DateTime BirthDate { get; set; }
 
-        public IFormFile ImageData { get; set; }
+        public IFormFile? ImageData { get; set; }
       
 
     }
